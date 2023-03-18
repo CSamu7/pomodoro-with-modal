2b7acc7e81734c0fba1119c32f671c24544201b0
CSamu7 <samuel.pdg@hotmail.com>
2023-03-17 21:25:14 -0600

    functions relate to Date now are here

---
+++ src/helpers/time.js
@@ -0,0 +1,21 @@
+const getFutureTime = (minutes) => {
+  const newTime = new Date();
+  newTime.setMinutes(newTime.getMinutes() + minutes);
+  return newTime;
+}
+
+const milisecondsToMinutes = (miliseconds) => {
+  return Math.floor(miliseconds / 1000 / 60);
+}
+
+const milisecondsToSeconds = (miliseconds) => {
+  return Math.floor((miliseconds / 1000) % 60);
+}
+
+const formatNumber = (number) => {
+  const stringNumber = number.toString()
+
+  return stringNumber.length === 1 ? `0${stringNumber}` : stringNumber
+}
+
+export {getFutureTime, milisecondsToMinutes, milisecondsToSeconds, formatNumber}
