11ac3f562c8db9b1ba710a2cd6f58f5822f892b7
CSamu7 <samuel.pdg@hotmail.com>
2023-03-26 15:17:38 -0600

    fix: fixed imports in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import "./App.css";
-import Header from "./components/Header";
-import Pomodoro from "./components/Pomodoro";
+import './App.css'
+import Header from './components/Header'
+import Pomodoro from './components/Pomodoro/Pomodoro'
 
 function App() {
   return (
-    <div className="App">
-      <Header title="Pomodoro v. 1.0"></Header>
-      <main className="wrapper">
+    <div className='App'>
+      <Header title='Pomodoro v. 1.0'></Header>
+      <main className='wrapper'>
         <Pomodoro initialTime={0.1}></Pomodoro>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
